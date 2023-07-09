464742b28b98624a256e218f6d939daacedb34fa
ryr0121 <ryr0121@gmail.com>
2023-07-10 00:49:39 +0900

    feat: 상단 앱바 UI 구현

---
+++ lib/common/layout/main_appbar_layout.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+import '../../shopping/view/search_screen.dart';
+import '../const/colors.dart';
+import '../view/root_tab.dart';
+
+/// 메인 로고와 검색창을 포함한 AppBar가 있는 화면 레이아웃 정의
+class MainAppBarLayout extends StatefulWidget {
+  final Widget widget;
+  final BuildContext context;
+  const MainAppBarLayout({required this.widget, required this.context, Key? key}) : super(key: key);
+
+  @override
+  State<MainAppBarLayout> createState() => _MainAppBarLayoutState();
+}
+
+class _MainAppBarLayoutState extends State<MainAppBarLayout> {
+  String? inputSearchKeyword;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: renderAppBar(),
+      body: widget.widget,
+    );
+  }
+
+  AppBar renderAppBar() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      leading: renderMainLogoButton(),
+      title: renderSearchField(),
+      elevation: 0,
+      leadingWidth: 90,
+    );
+  }
+
+  /// 메인 로고 아이콘 버튼 구현
+  Widget renderMainLogoButton() {
+    return Container(
+      height: 44,
+      width: 50,
+      padding: EdgeInsets.only(left: 18, top: 3, bottom: 2),
+      child: IconButton(
+        // TODO: png -> svg로 변경 (화질 이슈)
+        icon: Image.asset("asset/icons/common/main_logo.png"),
+        onPressed: () {
+          Navigator.of(context).pushAndRemoveUntil(
+              MaterialPageRoute(builder: (_) => RootTab()),
+                  (route) => false
+          );
+        },
+      )
+    );
+  }
+
+  /// 상단 검색창 구현
+  Widget renderSearchField() {
+    return Container(
+      height: 44,
+      padding: EdgeInsets.only(top: 3, bottom: 2),
+      child: TextField(
+        onTap: didTapSearchField(),
+        cursorColor: PURPLE_COLOR,
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.only(left: 20),
+          border: renderOutlineBorder(),
+          enabledBorder: renderOutlineBorder(),
+          focusedBorder: renderOutlineBorder(),
+          suffixIcon: Icon(Icons.search, color: PURPLE_COLOR,),
+        ),
+      ),
+    );
+  }
+
+  OutlineInputBorder renderOutlineBorder() {
+    return OutlineInputBorder(
+        borderSide: BorderSide(
+          color: PURPLE_COLOR,
+          width: 1.0,
+        ),
+        borderRadius: BorderRadius.circular(22)
+    );
+  }
+
+  /// 검색창 탭했을 경우 이벤트 정의
+  VoidCallback didTapSearchField() {
+    return () {
+      Navigator.of(context).push(MaterialPageRoute(builder: (_) => SearchScreen()));
+    };
+  }
+
+}
--- lib/shopping/view/shopping_screen.dart
+++ lib/shopping/view/shopping_screen.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 
+import '../../common/layout/main_appbar_layout.dart';
+
 class ShoppingScreen extends StatelessWidget {
   const ShoppingScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('쇼핑 화면'),
-      ),
+      body: MainAppBarLayout(widget: renderBody(), context: context,),
+    );
+  }
+
+  Widget renderBody() {
+    return Center(
+      child: Text('쇼핑 화면'),
     );
   }
 }
