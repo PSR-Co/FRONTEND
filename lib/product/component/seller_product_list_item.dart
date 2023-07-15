8d368a0a51ec1ca0c34b69bbbc5d25dd0f0528dc
ryr0121 <ryr0121@gmail.com>
2023-07-15 13:31:56 +0900

    feat: 판매자 프로필 화면 UI 구현

---
+++ lib/product/component/seller_product_list_item.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../../common/const/colors.dart';
+import '../view/product_detail_screen.dart';
+
+class SellerProductListItem extends StatefulWidget {
+  final String imgKey;
+  final String category;
+  final String name;
+  final int price;
+
+  const SellerProductListItem({
+    required this.imgKey,
+    required this.category,
+    required this.name,
+    required this.price,
+    Key? key
+  }) : super(key: key);
+
+  @override
+  State<SellerProductListItem> createState() => _SellerProductListItemState();
+}
+
+class _SellerProductListItemState extends State<SellerProductListItem> {
+  final cateogoryTextStyle = TextStyle(
+    fontSize: 14.0,
+    fontWeight: FontWeight.w500,
+    color: GRAY_4_COLOR,
+  );
+
+  final nameTextStyle = TextStyle(
+    fontSize: 12.0,
+    fontWeight: FontWeight.w400,
+    color: GRAY_4_COLOR,
+  );
+
+  final priceTextStyle = TextStyle(
+    fontSize: 14.0,
+    fontWeight: FontWeight.w500,
+    color: Colors.black,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: didTapItem,
+      child: IntrinsicHeight(
+        child: Container(
+          color: Colors.white,
+          padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+          child: Row(
+            children: [
+              renderProductImage(),
+              SizedBox(width: 10,),
+              renderProductInfo(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  /// rendering methods
+  Widget renderProductImage() {
+    return Container(
+        margin: EdgeInsets.symmetric(horizontal: 5.0),
+        decoration: BoxDecoration(
+          border: Border.all(
+              width: 1.0,
+              color: GRAY_0_COLOR
+          ),
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+        child: Image.asset(widget.imgKey, width: 90, height: 90, fit: BoxFit.cover,)
+    );
+  }
+
+  Widget renderProductInfo() {
+    return Expanded(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(widget.category, style: cateogoryTextStyle,),
+          SizedBox(height: 5,),
+          Text(widget.name, style: nameTextStyle,),
+          Text('${getTrimmedPrice()}원', style: priceTextStyle,),
+        ],
+      ),
+    );
+  }
+
+  /// helper methods
+  String getTrimmedPrice() {
+    return NumberFormat.currency(locale: 'ko_KR', symbol: '').format(widget.price);
+  }
+
+  /// event methods
+  void didTapItem() {
+    Navigator.of(context).push(MaterialPageRoute(builder: (_) => ProductDetailScreen(category: widget.category,)));
+  }
+}
--- lib/product/view/seller_profile_screen.dart
+++ lib/product/view/seller_profile_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:psr/common/const/colors.dart';
 import 'package:psr/common/layout/default_appbar_layout.dart';
 
+import '../component/seller_product_list_item.dart';
+
 class SellerProfileScreen extends StatefulWidget {
   const SellerProfileScreen({Key? key}) : super(key: key);
 
@@ -12,6 +14,8 @@ class SellerProfileScreen extends StatefulWidget {
 class _SellerProfileScreenState extends State<SellerProfileScreen> {
   static const PROFILE_IMG_SIZE = 64.0;
 
+  final int productCnt = 15;  // TODO: 패치 후 데이터 개수로 변경하기
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,40 +24,21 @@ class _SellerProfileScreenState extends State<SellerProfileScreen> {
     );
   }
 
+  /// rendering methods
   Widget renderBody() {
-    return Column(
-      children: [
-        renderSellerInfo(),
-        SizedBox(height: 5,),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width,
-              color: Colors.white,
-              padding: EdgeInsets.only(left: 15, top: 20, bottom: 15),
-              child: Text('글 목록', style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.w500,
-                color: Colors.black,
-            ),
-            ),
-            // ListView.builder(
-            //   itemCount: 10,
-            //     itemBuilder: (BuildContext context, int index) {
-            //       return Container(
-            //         height: 100,
-            //       );
-            //     }
-            ),
-          ],
-        ),
-
-      ],
+    return Container(
+      color: Colors.white,
+      child: ListView(
+        children: [
+          renderSellerInfoView(),
+          Container(height: 5, color: GRAY_0_COLOR.withOpacity(0.5),),
+          renderProductList(),
+        ],
+      ),
     );
   }
 
-  Widget renderSellerInfo() {
+  Widget renderSellerInfoView() {
     return Container(
       height: 110,
       width: MediaQuery.of(context).size.width,
@@ -105,6 +90,45 @@ class _SellerProfileScreenState extends State<SellerProfileScreen> {
     return Container(
       margin: EdgeInsets.symmetric(horizontal: 10),
       child: Text('루시 앤플 셀러', style: nameStyle,)
+    );
+  }
+
+  Widget renderProductList() {
+    final titleStyle = TextStyle(
+      fontSize: 22,
+      fontWeight: FontWeight.w500,
+      color: Colors.black,
+    );
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width,
+          color: Colors.white,
+          padding: EdgeInsets.only(left: 15, top: 20, bottom: 15),
+          child: Text('글 목록', style: titleStyle,),
+        ),
+        renderProductListView(),
+      ],
+    );
+  }
+
+  Widget renderProductListView() {
+    return Container(
+      height: 110.0 * (productCnt - 1),
+      child: ListView.builder(
+          physics: NeverScrollableScrollPhysics(),
+          itemCount: productCnt,
+          itemBuilder: (BuildContext context, int index) {
+            return SellerProductListItem(
+              imgKey: 'asset/images/product_sample.png',
+              category: '방송가능 아웃소싱',
+              name: '폴로랄프로렌 목도리 Red Color',
+              price: 79000,
+            );
+          }
+      ),
     );
   }
 }
