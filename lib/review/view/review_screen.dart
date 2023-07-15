bd2d0af67c79b88e72f4dd87821c4287b79cf7f0
ryr0121 <ryr0121@gmail.com>
2023-07-15 14:55:20 +0900

    feat: 리뷰 아이템 내 평점, 작성 정보, 신고 버튼 추가

--- lib/review/view/review_screen.dart
+++ lib/review/view/review_screen.dart
@@ -41,6 +41,61 @@ class _ReviewScreenState extends State<ReviewScreen> {
     return Container(
       color: Colors.white,
       height: 300,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          renderProfileImg(),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              renderAvgOfRating(), // 평점
+
+              Container(
+                margin: EdgeInsets.only(left: 14, top: 3),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // 아이디 (뒤4자리 가리기)
+                    Text('ryr0121', style: TextStyle(
+                      fontWeight: FontWeight.w700,
+                      fontSize: 12,
+                      color: GRAY_2_COLOR
+                    ),),
+
+                    SizedBox(width: 10,),
+                    // 작성일
+                    Text('23.07.16', style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 12,
+                      color: GRAY_2_COLOR,
+                    ),),
+
+                    SizedBox(width: 12,),
+                    // 신고 버튼
+                    TextButton(
+                        onPressed: (){},
+                        child: Text('신고', style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w500,
+                          color: GRAY_2_COLOR
+                        ),),
+                      style: TextButton.styleFrom(
+                        minimumSize: Size.zero,
+                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      ),
+                    )
+                  ],
+                ),
+              )
+
+              // 리뷰 내용
+
+              // 리뷰 사진 (사진 있으면,,!)
+            ],
+          ),
+        ],
+      ),
     );
   }
 
@@ -50,6 +105,49 @@ class _ReviewScreenState extends State<ReviewScreen> {
       width: MediaQuery.of(context).size.width-30,
       color: GRAY_1_COLOR.withOpacity(0.5),
       padding: EdgeInsets.symmetric(horizontal: 15),
+    );
+  }
+
+  Widget renderProfileImg() {
+    const double PROFILE_IMG_SIZE = 32.0;
+
+    return Container(
+      padding: EdgeInsets.only(left: 20, top: 15),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(PROFILE_IMG_SIZE/2),
+        child: Image.asset('asset/images/profile_img_sample.jpg', width: PROFILE_IMG_SIZE, height: PROFILE_IMG_SIZE,),
+      ),
+    );
+  }
+
+
+  Widget renderAvgOfRating() {
+    final reviewCntStyle = TextStyle(
+      fontSize: 12,
+      color: GRAY_4_COLOR,
+      fontWeight: FontWeight.w700,
+    );
+
+    final int avgOfRating = 5;
+
+    List<Widget> starIconList = [];
+    for (int i=0; i<avgOfRating; i++) {
+      starIconList.add(
+          Icon(Icons.star, color: Color(0xFFFFA939), size: 16.0,)
+      );
+    }
+    starIconList.add(
+        Container(
+            padding: EdgeInsets.symmetric(horizontal: 5),
+            child: Text('${avgOfRating.toDouble()}', style: reviewCntStyle)
+        )
+    );
+    return Container(
+      margin: EdgeInsets.only(left: 11, right: 14, top: 12),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: starIconList,
+      ),
     );
   }
 }
