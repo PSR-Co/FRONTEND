ae84742326b02b18f983aff1082342251720f4f5
ryr0121 <ryr0121@gmail.com>
2023-07-15 17:55:29 +0900

    feat: 요청하기 화면 내 bottom 버튼 추가

--- lib/order/view/add_order_screen.dart
+++ lib/order/view/add_order_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:psr/common/const/colors.dart';
 import 'package:psr/common/layout/default_appbar_layout.dart';
 
 class AddOrderScreen extends StatefulWidget {
@@ -13,6 +14,44 @@ class _AddOrderScreenState extends State<AddOrderScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: DefaultAppBarLayout(titleText: '요청하기',),
+      body: renderBody(),
+      bottomNavigationBar: renderBottomButton(),
     );
+  }
+
+  Widget renderBody() {
+    return Column(
+
+    );
+  }
+
+  Widget renderBottomButton() {
+    final titleStyle = TextStyle(
+      fontSize: 15,
+      fontWeight: FontWeight.w700,
+      color: Colors.white
+    );
+
+    return Container(
+      margin: EdgeInsets.only(left: 30, right: 30, bottom: 30),
+      height: 40.0,
+      child: ElevatedButton(
+        onPressed: didTapOrderButton,
+        child: Text('판매자에게 요청하기', style: titleStyle,),
+        style: ElevatedButton.styleFrom(
+          elevation: 0,
+          backgroundColor: PURPLE_COLOR,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          )
+        ),
+      ),
+    );
+  }
+
+  /// event methods
+  void didTapOrderButton() {
+    // TODO: 작성된 요청 검토 및 POST
+    print("didTapOrderButton");
   }
 }
