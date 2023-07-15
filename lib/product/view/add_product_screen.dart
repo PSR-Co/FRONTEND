74167f8a027ea9d154aa5b6d67a0e073ae4ffe66
ryr0121 <ryr0121@gmail.com>
2023-07-15 23:28:42 +0900

    feat: 상품 추가 중 사진 선택 위젯 추가(최대 5장 제한 포함)

--- lib/product/view/add_product_screen.dart
+++ lib/product/view/add_product_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:psr/common/layout/default_appbar_layout.dart';
 import 'package:psr/common/layout/purple_bottom_button.dart';
+import 'dart:io';
 
 import '../../common/const/colors.dart';
 import '../../common/layout/purple_outlined_text_field.dart';
@@ -16,6 +19,7 @@ class AddProductScreen extends StatefulWidget {
 class _AddProductScreenState extends State<AddProductScreen> {
 
   List<String> imgKeyList = [];
+  String? selectedCategory;
 
   final TextEditingController nameController = TextEditingController();
   final TextEditingController priceController = TextEditingController();
@@ -32,12 +36,22 @@ class _AddProductScreenState extends State<AddProductScreen> {
   }
 
   Widget renderBody() {
+    final defaultStyle = OutlineInputBorder(
+        borderRadius: BorderRadius.circular(14),
+        borderSide: BorderSide(
+          color: PURPLE_COLOR.withOpacity(0.5),
+          width: 1.0,
+        )
+    );
+
     return ListView(
       children: [
         SizedBox(height: 20,),
 
         renderTitleText('상품 카테고리', null),
         // 카테고리 드롭다운
+
+
         SizedBox(height: 15,),
 
 
@@ -72,20 +86,39 @@ class _AddProductScreenState extends State<AddProductScreen> {
             Container(
               height: 100,
               padding: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
-              child: ListView(
+              child: ListView.builder(
                 scrollDirection: Axis.horizontal,
-                children: [
-                  // IconButton(onPressed: (){}, icon: icon),
-                  SvgPicture.asset('asset/icons/shopping/pick_img_icon.svg', width: 90, height: 90,)
-                ],
+                itemCount: imgKeyList.length + 1,
+                itemBuilder: (BuildContext context, int index) {
+                  print('index - ${index}');
+                  if (imgKeyList.length > 0 && index < imgKeyList.length) {
+                    return Container(
+                        padding: EdgeInsets.all(5),
+                        child: Image.file(File(imgKeyList.elementAt(index)), width: 90, height: 90,)
+                    );
+                  } else {
+                    return SizedBox(
+                      width: 90,
+                      height: 90,
+                      child: IconButton(
+                          padding: EdgeInsets.zero,
+                          onPressed: didTapPickImgButton,
+                          icon: SvgPicture.asset('asset/icons/shopping/pick_img_icon.svg',)
+                      ),
+                    );
+                  }
+                },
+                // children: renderImgListView(),
               ),
             ),
             Container(
-              padding: EdgeInsets.symmetric(horizontal: 18),
-              child: Text('상품과 무관한 사진을 첨부하면 노출 제한 처리 될 수 있습니다. \n사진 첨부시 개인 정보가 노출되지 않게 유의해주세요.', style: TextStyle(
-                fontSize: 11,
-                color: GRAY_1_COLOR,
-              )),
+              padding: EdgeInsets.symmetric(horizontal: 18, vertical: 3),
+              child: Text('상품과 무관한 사진을 첨부하면 노출 제한 처리 될 수 있습니다. \n사진 첨부시 개인 정보가 노출되지 않게 유의해주세요.',
+                  style: TextStyle(
+                    fontSize: 11,
+                    color: GRAY_1_COLOR,
+                  )
+              ),
             )
           ],
         ),
@@ -93,6 +126,33 @@ class _AddProductScreenState extends State<AddProductScreen> {
 
       ],
     );
+  }
+
+  List<Widget> renderImgListView() {
+    print("renderImgListView called -> imgKeyList.length : ${imgKeyList.length}");
+    List<Widget> imgs = [];
+    if (imgKeyList.length > 0) {
+      imgKeyList.map((e) =>
+          imgs.add(
+            Container(
+              padding: EdgeInsets.all(5),
+              child: Image.file(File(e), width: 90, height: 90,)
+            ),
+          )
+      );
+    }
+    imgs.add(
+      SizedBox(
+        width: 90,
+        height: 90,
+        child: IconButton(
+            padding: EdgeInsets.zero,
+            onPressed: didTapPickImgButton,
+            icon: SvgPicture.asset('asset/icons/shopping/pick_img_icon.svg',)
+        ),
+      ),
+    );
+    return imgs;
   }
 
   Widget renderTitleText(String title, String? option) {
@@ -124,6 +184,24 @@ class _AddProductScreenState extends State<AddProductScreen> {
   /// event methods
   void didTapAddButton() {
     print("didTapAddButton");
+  }
+
+  void didTapPickImgButton() async {
+    if (imgKeyList.length < 5) {
+      var picker = ImagePicker();
+      var image = await picker.pickImage(source: ImageSource.gallery);
+      if (image != null) {
+        setState(() {
+          imgKeyList.add(image.path);
+        });
+      }
+    } else {
+      Fluttertoast.showToast(
+          msg: '사진은 최대 5개까지 첨부할 수 있습니다.',
+          gravity: ToastGravity.BOTTOM
+      );
+    }
+
   }
 
 }
