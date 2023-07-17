5a453eef5839058e799db07faf5deca94681a60d
ryr0121 <ryr0121@gmail.com>
2023-07-17 11:44:06 +0900

    feat: 스플래시 화면 및 로직 추가

--- lib/common/view/splash_screen.dart
+++ lib/common/view/splash_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:psr/common/view/root_tab.dart';
 import 'package:psr/auth/view/login_screen.dart';
 
-import '../../auth/view/login_screen.dart';
+import '../const/constants.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -19,25 +19,30 @@ class _SplashScreenState extends State<SplashScreen> {
     checkToken();
   }
 
-  void checkToken() {
+  void checkToken() async {
+
+    final refreshToken = await storage.read(key: REFRESH_TOKEN_KEY);
+    final accessToken = await storage.read(key: ACCESS_TOKEN_KEY);
+
     // TODO: Access / Refresh Token 저장 여부 확인 (있으면 홈 화면, 없으면 로그인 화면으로 전환)
-    if (false) {
+    if (refreshToken == null && accessToken == null) {
       Navigator.of(context).pushAndRemoveUntil(
-          MaterialPageRoute(builder: (_) => LoginScreen()
-          ), (route) => false);
+          MaterialPageRoute(builder: (_) => LoginScreen()),
+              (route) => false);
+
     } else {
       Navigator.of(context).pushAndRemoveUntil(
           MaterialPageRoute(builder: (_) => RootTab()
           ), (route) => false);
+
     }
+
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("Splash Screen"),
-      ),
-    );
+    return Center(
+        child: Image.asset('asset/icons/splash/splash_logo.png', ),
+      );
   }
 }
