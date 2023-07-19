9a64d5bc4ac0b60ee259d60f0cf7351a69a6fc54
ryr0121 <ryr0121@gmail.com>
2023-07-19 23:39:31 +0900

    feat: 비밀번호 재설정 화면 UI 구현

--- lib/auth/view/find_id_screen.dart
+++ lib/auth/view/find_id_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:psr/auth/component/input_user_info.dart';
+import 'package:psr/auth/view/login_screen.dart';
 
 import '../../common/layout/default_appbar_layout.dart';
+import '../../common/layout/purple_filled_button.dart';
+import '../component/show_user_id.dart';
 
 class FindIDScreen extends StatefulWidget {
   const FindIDScreen({Key? key}) : super(key: key);
@@ -10,11 +14,53 @@ class FindIDScreen extends StatefulWidget {
 }
 
 class _FindIDScreenState extends State<FindIDScreen> {
+  bool isFounded = false;
+
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController phoneNumController = TextEditingController();
+  final TextEditingController codeController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
-      appBar: DefaultAppBarLayout(titleText: '아이디 찾기',),
+      appBar: const DefaultAppBarLayout(titleText: '아이디 찾기',),
+      body: renderBody(),
+      bottomNavigationBar: PurpleFilledButton(
+        title: (isFounded) ? '로그인' : '다음',
+        onPressed: didTapNextButton,
+        height: 40,
+      ),
     );
   }
+
+  Widget renderBody() {
+    return (isFounded)
+        ? const ShowUserID()
+        : const Column(
+          children: [
+            SizedBox(height: 40,),
+            InputUserInfo(),
+          ],
+        );
+  }
+
+
+  /// event methods
+  void didTapNextButton() {
+    setState(() {
+      if (isFounded) {
+        Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(
+                builder: (_) => const LoginScreen()
+            ), (route) => false
+        );
+      }
+      isFounded = !isFounded;
+    });
+  }
+
+  // void didTapSendCodeButton() {
+  //   print('didTapSendCodeButton - 인증번호 요청');
+  // }
 }
