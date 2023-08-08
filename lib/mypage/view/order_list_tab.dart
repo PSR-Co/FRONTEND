db6d8c4efd643b5248720da571afa9749c5f7369
seuriseuljjeok <heeseul09@naver.com>
2023-08-09 01:36:16 +0900

    feat: 마이페이지 내 요청목록의 요청카드와 상세화면 연결

--- lib/mypage/view/order_list_tab.dart
+++ lib/mypage/view/order_list_tab.dart
@@ -4,6 +4,7 @@ import 'package:psr/common/const/constants.dart';
 import 'package:psr/common/layout/large_detail_bar_layout.dart';
 import 'package:psr/common/view/body_tab.dart';
 import 'package:psr/mypage/view/orderlist_screen.dart';
+import 'package:psr/mypage/view/seller_detail_order_screen.dart';
 
 import '../../common/const/colors.dart';
 import '../../home/component/card_slider.dart';
@@ -13,16 +14,20 @@ import '../component/order_list_item.dart';
 class OrderListTab extends StatefulWidget {
   MyPage myPageData;
 
-  OrderListTab({required this.myPageData, Key? key}): super(key: key);
+  OrderListTab({required this.myPageData, Key? key}) : super(key: key);
 
   @override
   State<OrderListTab> createState() => _OrderListTabState();
 }
 
-class _OrderListTabState extends State<OrderListTab> with SingleTickerProviderStateMixin{
-  final TextStyle orderTextStyle = const TextStyle(fontSize: 14.0, fontWeight: FontWeight.w400, color: GRAY_4_COLOR);
-  final TextStyle orderProductTextStyle = const TextStyle(fontSize: 15.0, fontWeight: FontWeight.w500, color: GRAY_4_COLOR);
-  final TextStyle orderDateTextStyle = const TextStyle(fontSize: 13.0, fontWeight: FontWeight.w400, color: GRAY_2_COLOR);
+class _OrderListTabState extends State<OrderListTab>
+    with SingleTickerProviderStateMixin {
+  final TextStyle orderTextStyle = const TextStyle(
+      fontSize: 14.0, fontWeight: FontWeight.w400, color: GRAY_4_COLOR);
+  final TextStyle orderProductTextStyle = const TextStyle(
+      fontSize: 15.0, fontWeight: FontWeight.w500, color: GRAY_4_COLOR);
+  final TextStyle orderDateTextStyle = const TextStyle(
+      fontSize: 13.0, fontWeight: FontWeight.w400, color: GRAY_2_COLOR);
 
   final controller = SliderController();
 
@@ -45,12 +50,21 @@ class _OrderListTabState extends State<OrderListTab> with SingleTickerProviderSt
     List<SellProduct> sellProductList = widget.myPageData.sellList;
     List<OrderProduct> orderProductList = widget.myPageData.orderList;
 
-    return BodyTab(tabTitle: LargeDetailBar(title: "요청목록", moveTo: OrderListScreen(sellProductList: sellProductList, orderProductList: orderProductList)),
-      tabBarViewChild: [sellProductCardSlider(productList: sellProductList), orderProductCardSlider(productList: orderProductList)],
-      titleList: ORDER_LIST_TAB,);
+    return BodyTab(
+      tabTitle: LargeDetailBar(
+          title: "요청목록",
+          moveTo: OrderListScreen(
+              sellProductList: sellProductList,
+              orderProductList: orderProductList)),
+      tabBarViewChild: [
+        sellProductCardSlider(productList: sellProductList),
+        orderProductCardSlider(productList: orderProductList)
+      ],
+      titleList: ORDER_LIST_TAB,
+    );
   }
 
-  Widget sellProductCardSlider({required List<SellProduct> productList}){
+  Widget sellProductCardSlider({required List<SellProduct> productList}) {
     return SizedBox(
       width: MediaQuery.of(context).size.width,
       child: WidgetSlider(
@@ -60,13 +74,17 @@ class _OrderListTabState extends State<OrderListTab> with SingleTickerProviderSt
         proximity: 0.53,
         sizeDistinction: 0.4,
         itemBuilder: (context, index, activeIndex) {
-          return cardContent(productList[index].ordererName, productList[index].productImgKey, productList[index].productName, productList[index].orderDate);
+          return cardContent(
+              productList[index].ordererName,
+              productList[index].productImgKey,
+              productList[index].productName,
+              productList[index].orderDate);
         },
       ),
     );
   }
 
-  Widget orderProductCardSlider({required List<OrderProduct> productList}){
+  Widget orderProductCardSlider({required List<OrderProduct> productList}) {
     return SizedBox(
       width: MediaQuery.of(context).size.width,
       child: WidgetSlider(
@@ -76,47 +94,84 @@ class _OrderListTabState extends State<OrderListTab> with SingleTickerProviderSt
         proximity: 0.53,
         sizeDistinction: 0.4,
         itemBuilder: (context, index, activeIndex) {
-          return cardContent(productList[index].sellerName, productList[index].productImgKey, productList[index].productName, productList[index].orderDate);
+          return cardContent(
+              productList[index].sellerName,
+              productList[index].productImgKey,
+              productList[index].productName,
+              productList[index].orderDate);
         },
       ),
     );
   }
 
-  Widget cardContent(String ordererName, String productImg, String productName, String orderDate){
+  Widget cardContent(String ordererName, String productImg, String productName, String orderDate) {
     return GestureDetector(
-      onTap: () {},
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => (SellerDetailOrderSccreen(
+                      selectedValue: '',
+                      orderDate: orderDate,
+                      productName: productName,
+                      btnOption1: '요청승인',
+                      btnOption2: '요청거절',
+                    ))));
+      },
       child: Container(
         margin: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 0.0),
         decoration: const BoxDecoration(
             boxShadow: [
-              BoxShadow(color: LIGHT_SHADOW_COLOR,
+              BoxShadow(
+                color: LIGHT_SHADOW_COLOR,
                 spreadRadius: 0,
                 blurRadius: 4,
               ),
             ],
             shape: BoxShape.rectangle,
-            borderRadius: BorderRadius.all(Radius.circular(20))
-        ),
+            borderRadius: BorderRadius.all(Radius.circular(20))),
         child: Container(
-            decoration: BoxDecoration(color: Colors.white, shape: BoxShape.rectangle, borderRadius: const BorderRadius.all(Radius.circular(20))),
+            decoration: const BoxDecoration(
+                color: Colors.white,
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.all(Radius.circular(20))),
             child: Center(
               child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Container(margin:EdgeInsets.only(left: 8.0),child: Text("${ordererName}님의 요청", style: orderTextStyle,)),
+                    Container(
+                        margin: const EdgeInsets.only(left: 8.0),
+                        child: Text(
+                          "$ordererName님의 요청",
+                          style: orderTextStyle,
+                        )),
                     Container(
                         width: 170.0,
                         height: 170.0,
-                        margin: EdgeInsets.only(top: 15.0, bottom: 10.0),
-                        decoration: BoxDecoration(border: Border.all(color: GRAY_0_COLOR, width: 1.0, style: BorderStyle.solid)),
-                        child: ClipRect(child: Image.asset(productImg, width: 165.0, height: 165.0, ),)),
-                    Text(productName, style: orderProductTextStyle,),
-                    Text(orderDate, style: orderDateTextStyle,)
-                  ]
-              ),
-            )
-        ),
+                        margin: const EdgeInsets.only(top: 15.0, bottom: 10.0),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                                color: GRAY_0_COLOR,
+                                width: 1.0,
+                                style: BorderStyle.solid)),
+                        child: ClipRect(
+                          child: Image.asset(
+                            productImg,
+                            width: 165.0,
+                            height: 165.0,
+                          ),
+                        )),
+                    Text(
+                      productName,
+                      style: orderProductTextStyle,
+                    ),
+                    Text(
+                      orderDate,
+                      style: orderDateTextStyle,
+                    )
+                  ]),
+            )),
       ),
     );
   }
