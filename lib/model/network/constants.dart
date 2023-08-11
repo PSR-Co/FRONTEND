c4d1aec478e426d14c15d3de04b09ed74e82a691
ryr0121 <ryr0121@gmail.com>
2023-08-11 19:33:17 +0900

    fix: Base URL 수정

--- lib/model/network/constants.dart
+++ lib/model/network/constants.dart
@@ -2,7 +2,7 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 /// BASE URL
-const BASE_URL = 'http://3.38.179.128:8080';
+const BASE_URL = 'http://43.202.16.213:8080';
 
 /// token 로컬 저장을 위한 키값
 const ACCESS_TOKEN_KEY = 'ACCESS_TOKEN';
