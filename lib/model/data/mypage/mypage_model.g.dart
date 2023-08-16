65960f7555bb3b9e3f0e5717abc84af5510c811a
seuriseuljjeok <heeseul09@naver.com>
2023-08-16 23:45:08 +0900

    rename: myproduct model 파일명 변경

--- lib/model/data/mypage/mypage_model.g.dart
+++ lib/model/data/mypage/mypage_model.g.dart
@@ -1,49 +1,49 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'myproduct_model.dart';
+part of 'mypage_model.dart';
 
 // **************************************************************************
 // JsonSerializableGenerator
 // **************************************************************************
 
-MyProductModel _$MyProductModelFromJson(Map<String, dynamic> json) =>
-    MyProductModel(
+MyPageModel _$MyPageModelFromJson(Map<String, dynamic> json) =>
+    MyPageModel(
       code: json['code'] as int,
       message: json['message'] as String,
-      data: MyProductData.fromJson(json['data'] as Map<String, dynamic>),
+      data: MyPageData.fromJson(json['data'] as Map<String, dynamic>),
     );
 
-Map<String, dynamic> _$MyProductModelToJson(MyProductModel instance) =>
+Map<String, dynamic> _$MyProductModelToJson(MyPageModel instance) =>
     <String, dynamic>{
       'code': instance.code,
       'message': instance.message,
       'data': instance.data,
     };
 
-MyProductData _$MyProductDataFromJson(Map<String, dynamic> json) =>
-    MyProductData(
-      productList: MyProductContent.fromJson(
+MyPageData _$MyPageDataFromJson(Map<String, dynamic> json) =>
+    MyPageData(
+      productList: MyPageContent.fromJson(
           json['productList'] as Map<String, dynamic>),
     );
 
-Map<String, dynamic> _$MyProductDataToJson(MyProductData instance) =>
+Map<String, dynamic> _$MyProductDataToJson(MyPageData instance) =>
     <String, dynamic>{
       'productList': instance.productList,
     };
 
-MyProductContent _$MyProductContentFromJson(Map<String, dynamic> json) =>
-    MyProductContent(
+MyPageContent _$MyPageContentFromJson(Map<String, dynamic> json) =>
+    MyPageContent(
       content: (json['content'] as List<dynamic>)
-          .map((e) => MyProduct.fromJson(e as Map<String, dynamic>))
+          .map((e) => MyPage.fromJson(e as Map<String, dynamic>))
           .toList(),
     );
 
-Map<String, dynamic> _$MyProductContentToJson(MyProductContent instance) =>
+Map<String, dynamic> _$MyProductContentToJson(MyPageContent instance) =>
     <String, dynamic>{
       'content': instance.content,
     };
 
-MyProduct _$MyProductFromJson(Map<String, dynamic> json) => MyProduct(
+MyPage _$MyPageFromJson(Map<String, dynamic> json) => MyPage(
       productId: json['productId'] as int,
       imgUrl: json['imgUrl'] as String?,
       category: json['category'] as String,
@@ -51,7 +51,7 @@ MyProduct _$MyProductFromJson(Map<String, dynamic> json) => MyProduct(
       price: json['price'] as int,
     );
 
-Map<String, dynamic> _$MyProductToJson(MyProduct instance) => <String, dynamic>{
+Map<String, dynamic> _$MyProductToJson(MyPage instance) => <String, dynamic>{
       'productId': instance.productId,
       'imgUrl': instance.imgUrl,
       'category': instance.category,
