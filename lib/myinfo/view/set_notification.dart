a1eeb50b51a4a515c80c2bdafecc775b17a52200
ryr0121 <ryr0121@gmail.com>
2023-09-10 04:32:09 +0900

    feat: 마이페이지 내 유저 알림 권한 받아오기 추가

--- lib/myinfo/view/set_notification.dart
+++ lib/myinfo/view/set_notification.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 import '../../common/const/colors.dart';
 
@@ -17,30 +19,43 @@ class _SetNotificationState extends State<SetNotification> {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 50.0,
-      child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              "알림받기",
-              style: headerTextStyle,
-            ),
-            CupertinoSwitch(
-                value: isChecked,
-                activeColor: PURPLE_COLOR,
-                onChanged: (value) {
-                  changeChecked();
-                })
-          ]),
+    return FutureBuilder<dynamic> (
+        future: configure(),
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          return SizedBox(
+            height: 50.0,
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    "알림받기",
+                    style: headerTextStyle,
+                  ),
+                  CupertinoSwitch(
+                      value: isChecked,
+                      activeColor: PURPLE_COLOR,
+                      onChanged: (value) {
+                        changeChecked();
+                      })
+                ]),
+          );
+        }
     );
+
   }
 
-  void changeChecked() {
-    setState(() {
-      isChecked = !isChecked;
+  Future<void> configure() async {
+    isChecked = await Permission.notification.isGranted;
+  }
+
+  Future<void> changeChecked() async {
+    await Permission.notification.request();
+    bool isGranted = await Permission.notification.isGranted;
+    setState(()  {
+      // isChecked = !isChecked;
+      isChecked = isGranted;
+      print('changeChecked called :: isGranted -> $isGranted');
     });
   }
-
 }
